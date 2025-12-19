72b35bda73c7a16f4901af47fab5362a823069f9
Piyush K <piyushtirpude@gmail.com>
2025-12-19 13:10:43 +0530

    initail project setup done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <header>
+        <h1>Queue Management Application</h1>
+        <p>Manage your Customers Effeiciently</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
